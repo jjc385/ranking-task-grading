251509842e3a973fd950ca312ef846dded07d694
Jared Claypoole <jjc385@ucla.edu>
2017-10-29 20:19:02 -0700

    allow for multiple correct answers
    * Allows for more than one correct answer
      * If more than one correct answer is supplied,
    the correct answer which generates the maximum
    student score is used

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from scipy.stats import kendalltau
 
 problem_point_value = 3
 
-correct_string = "ABCDEF"
+# All of the strings in this list are correct.
+correct_strings = [
+        "ABCDEF"
+        ]
 
 
 #### Code
@@ -15,30 +18,37 @@ def grade_ranking_task() :
     student_string = input( "Input the student's answer: " ).strip().upper()
 
 
-    correct_answers = [letter for letter in correct_string]
-    student_answers = [letter for letter in student_string]
+    correct_answers = [[letter for letter in correct_string] for correct_string in correct_strings ]
+    student_answer = [letter for letter in student_string]
 
 
-    if sorted(student_answers) != sorted(correct_answers) :
+    correct_form = False
+    for correct_answer in correct_answers :
+        if sorted(student_answer) == sorted(correct_answer) :
+            correct_form = True
+            break
+    if not correct_form :
         print("Warning:  student answer is NOT a rearrangement of the correct answer")
 
 
-    cypher = { letter:(i+1) for i,letter in enumerate(correct_answers) }
+    tau_max = 0.0
+    for correct_answer in correct_answers :
+        cypher = { letter:(i+1) for i,letter in enumerate(correct_answer) }
 
-    student_numerical = [cypher[letter] for letter in student_answers]
+        student_numerical = [cypher[letter] for letter in student_answer]
 
-    tau = kendalltau( [range(1,len(correct_answers)+1)], student_numerical)[0]
+        tau = kendalltau( [range(1,len(correct_answer)+1)], student_numerical)[0]
 
-    if tau > 0:
-        print("Student Score:", ceil((tau * problem_point_value) * 10) / 10)
-    else:
-        print("Student Score:", 0.0)
+        if tau > tau_max :
+            tau_max = tau
+
+    print("Student Score:", ceil((tau_max * problem_point_value) * 10) / 10)
 
 
 if __name__ == '__main__' :
 
     print( "This question is worth:\t", problem_point_value, "points" )
-    print( "The correct answer is:\t", correct_string )
+    print( "The possible correct answers are:\t", correct_strings )
     print( "Note that student answers are not case sensitive.\n\n" )
 
     while True :
--- readme.md
+++ readme.md
@@ -11,3 +11,6 @@ Some updates made by Jared.  The script now :
 * Runs in an infinite loop so that the script need not be rerun for every student
 * Warns the grader if the student answer is not a rearrangement of the correct answer
 * Prints point value and correct answer at the start to remind the grader
+* Allows for more than one correct answer
+  * If more than one correct answer is supplied, the correct answer which
+	generates the maximum student score is used
