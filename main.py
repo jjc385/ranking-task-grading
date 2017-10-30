45e8fac6f3b35c151836824a7301e708d09330c9
Jared Claypoole <jjc385@ucla.edu>
2017-10-29 18:36:00 -0700

    user friendliness
    * Now prompts the grader for input
    * Runs in an infinite loop so that the script need not be rerun for
    every student

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
-# Grader Inputs
+from math import ceil
+from scipy.stats import kendalltau
+
+#### Grader Inputs
 
 problem_point_value = 3
 
 correct_string = "ABCDEF"
-student_string = "ABCDEF"
 
-# Code
 
-from math import ceil
-from scipy.stats import kendalltau
+#### Code
 
-correct_answers = [letter for letter in correct_string]
-student_answers = [letter for letter in student_string]
+def grade_ranking_task() :
 
-cypher = { letter:i for i,letter in enumerate(correct_answers) }
+    student_string = input( "Input the student's answer: " ).strip().upper()
 
-student_numerical = [cypher[letter] for letter in student_answers]
 
-tau = kendalltau( [range(1,len(correct_answers)+1)], student_numerical)[0]
+    correct_answers = [letter for letter in correct_string]
+    student_answers = [letter for letter in student_string]
 
-if tau > 0:
-    print("Student Score:", ceil((tau * problem_point_value) * 10) / 10)
-else:
-    print("Student Score:", 0.0)
+    cypher = { letter:i for i,letter in enumerate(correct_answers) }
+
+    student_numerical = [cypher[letter] for letter in student_answers]
+
+    tau = kendalltau( [range(1,len(correct_answers)+1)], student_numerical)[0]
+
+    if tau > 0:
+        print("Student Score:", ceil((tau * problem_point_value) * 10) / 10)
+    else:
+        print("Student Score:", 0.0)
+
+
+if __name__ == '__main__' :
+
+    print( "The correct answer is:  ", correct_string )
+    print( "Note that student answers are not case sensitive.\n\n" )
+
+    while True :
+        grade_ranking_task()
+        print()
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ Josh Samani's script for grading ranking tasks
 Some updates made by Jared
 
 * Updated to work for an answer string of any length
+* Now prompts the grader for input
+* Runs in an infinite loop so that the script need not be rerun for every student
