8953eef65b8ce72c36d53f41f407b5775086c977
Jared Claypoole <jjc385@ucla.edu>
2017-10-29 18:13:23 -0700

    works for answer string of any length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Grader Inputs
 
-problem_point_value = 5
+problem_point_value = 3
 
 correct_string = "ABCDEF"
 student_string = "ABCDEF"
@@ -13,11 +13,11 @@ from scipy.stats import kendalltau
 correct_answers = [letter for letter in correct_string]
 student_answers = [letter for letter in student_string]
 
-cypher = {correct_answers[0]:1, correct_answers[1]:2, correct_answers[2]:3, correct_answers[3]:4, correct_answers[4]:5, correct_answers[5]:6}
+cypher = { letter:i for i,letter in enumerate(correct_answers) }
 
 student_numerical = [cypher[letter] for letter in student_answers]
 
-tau = kendalltau([1,2,3,4,5,6], student_numerical)[0]
+tau = kendalltau( [range(1,len(correct_answers)+1)], student_numerical)[0]
 
 if tau > 0:
     print("Student Score:", ceil((tau * problem_point_value) * 10) / 10)
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ## ranking-task-grading
 
 Josh Samani's script for grading ranking tasks
+
+Some updates made by Jared
+
+* Updated to work for an answer string of any length
